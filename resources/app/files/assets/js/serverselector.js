02f390499cb9a3e58dab0a962cf6ac4d5de85074
CakeLancelot <CakeLancelot@users.noreply.github.com>
2020-12-23 04:05:29 -0600

    Adding and deleting servers works.
    Still need to fix the add server validation
    Other changes:
    * Convert all files to spaces and fix indentation
    * We now write out the images.php and sponsor.php files if an endpoint url is defined for a server
    * Added comments

--- resources/app/files/assets/js/serverselector.js
+++ resources/app/files/assets/js/serverselector.js
@@ -19,6 +19,34 @@ function disableServerListButtons() {
   $('#of-deleteserver-button').prop('disabled', true);
 }
 
+function addServer() {
+  var jsontomodify = JSON.parse(remotefs.readFileSync(userdir+"\\servers.json"));
+
+  var server = {};
+  server['uuid'] = uuidv4();
+  server['description'] = $("#addserver-descinput").val();
+  server['ip'] = $("#addserver-ipinput").val();
+  server['version'] = $("#addserver-versionselect option:selected").text();
+  //server['endpoint'] = 
+
+  jsontomodify['servers'].push(server)
+
+  remotefs.writeFileSync(userdir+"\\servers.json", JSON.stringify(jsontomodify, null, 4));
+  loadServerList();
+}
+
+function deleteServer() {
+  var jsontomodify = JSON.parse(remotefs.readFileSync(userdir+"\\servers.json"));
+  var result = jsontomodify['servers'].filter(function(obj) {return (obj.uuid === getSelectedServer())})[0];
+
+  var resultindex = jsontomodify['servers'].indexOf(result);
+
+  jsontomodify['servers'].splice(resultindex, 1);
+
+  remotefs.writeFileSync(userdir+"\\servers.json", JSON.stringify(jsontomodify, null, 4));
+  loadServerList();
+}
+
 function loadGameVersions() {
   var versionjson = JSON.parse(remotefs.readFileSync(userdir+"\\versions.json"));
   versionarray = versionjson['versions'];
@@ -33,30 +61,32 @@ function loadConfig() {
   }
 
 function loadServerList() {
-    var serverjson = JSON.parse(remotefs.readFileSync(userdir+"\\servers.json"));
-    serverarray = serverjson['servers'];
+  var serverjson = JSON.parse(remotefs.readFileSync(userdir+"\\servers.json"));
+  serverarray = serverjson['servers'];
 
-    $(".server-listing-entry").remove(); // clear out old stuff, if any
-    disableServerListButtons();
+  $(".server-listing-entry").remove(); // Clear out old stuff, if any
+  disableServerListButtons(); // Disable buttons until another is selected
 
-    if (serverarray.length > 0) {
-      $("#server-listing-placeholder").attr("hidden",true);
-      $.each(serverarray, function( key, value ) {
-        var row = document.createElement('tr');
-        row.className = 'server-listing-entry'
-        row.setAttribute('id', value.uuid)
-        var cellName = document.createElement('td');
-        cellName.textContent = value.description
-        var cellVersion = document.createElement('td');
-        cellVersion.textContent = value.version
-        cellVersion.className = 'text-monospace'
+  if (serverarray.length > 0) {
+    // Servers were found in the JSON
+    $("#server-listing-placeholder").attr("hidden",true);
+    $.each(serverarray, function( key, value ) {
+      // Create the row, and populate the cells
+      var row = document.createElement('tr');
+      row.className = 'server-listing-entry'
+      row.setAttribute('id', value.uuid);
+      var cellName = document.createElement('td');
+      cellName.textContent = value.description
+      var cellVersion = document.createElement('td');
+      cellVersion.textContent = value.version
+      cellVersion.className = 'text-monospace'
 
-        row.appendChild(cellName);
-        row.appendChild(cellVersion);
-        document.getElementById('server-tablebody').appendChild(row);
-      });
-    } else {
-    // no servers added, make sure placeholder is visible
+      row.appendChild(cellName);
+      row.appendChild(cellVersion);
+      $("#server-tablebody").append(row);
+    });
+  } else {
+    // No servers are added, make sure placeholder is visible
     $("#server-listing-placeholder").attr("hidden",false);
   }
 }
@@ -71,33 +101,38 @@ function setGameInfo(serverUUID) {
   remotefs.writeFileSync(__dirname+"\\loginInfo.php", result.ip);
 
   if (result.hasOwnProperty('endpoint')) {
-    var rankurl = result.endpoint.replace("https://", "http://") + "getranks"
-    console.log("Using rank endpoint " + rankurl)
-    remotefs.writeFileSync(__dirname+"\\rankurl.txt", rankurl);
+    var httpendpoint = result.endpoint.replace("https://", "http://")
+    remotefs.writeFileSync(__dirname+"\\rankurl.txt", httpendpoint+"getranks");
+    // Write these out too
+    remotefs.writeFileSync(__dirname+"\\sponsor.php", httpendpoint+"upsell/sponsor.png");
+    remotefs.writeFileSync(__dirname+"\\images.php", httpendpoint+"upsell/");
   } else {
     if (remotefs.existsSync(__dirname+"\\rankurl.txt")) {
       // delete the file, this server won't be using it
       remotefs.unlinkSync(__dirname+"\\rankurl.txt");
+      remotefs.writeFileSync(__dirname+"\\sponsor.php", "assets/img/welcome.png");
+      remotefs.writeFileSync(__dirname+"\\images.php", "assets/img/");
     }
   }
 }
 
+// Returns the UUID of the server with the selected background color.
+// Yes, there are probably better ways to go about this, but it works well enough.
 function getSelectedServer() {
-  return $("tr.bg-primary").prop("id");
+  return $("#server-tablebody > tr.bg-primary").prop("id");
 }
 
 function connectToServer() {
   // Get ID of the selected server, which corresponds to its UUID in the json
-  var uuid = getSelectedServer();
-  console.log("Connecting to server with UUID of " + uuid);
+  console.log("Connecting to server with UUID of " + getSelectedServer());
 
-  // prevent the user from clicking anywhere else during the transition
+  // Prevent the user from clicking anywhere else during the transition
   $('body,html').css('pointer-events','none');
   stopEasterEggs();
   $('#of-serverselector').fadeOut('slow', function() {
     setTimeout(function(){
       $('body,html').css('pointer-events','');
-      setGameInfo(uuid);
+      setGameInfo(getSelectedServer());
       launchGame();
     }, 200);
   });
@@ -111,4 +146,9 @@ $('#server-table').on('click', '.server-listing-entry', function(event) {
 $('#server-table').on('dblclick', '.server-listing-entry', function(event) {
   $(this).addClass('bg-primary').siblings().removeClass('bg-primary');
   connectToServer();
+});
+
+$('#of-deleteservermodal').on('show.bs.modal', function (e) {
+  var result = serverarray.filter(function(obj) {return (obj.uuid === getSelectedServer());})[0];
+  $("#deleteserver-servername").html(result.description);
 });
--- resources/app/index.js
+++ resources/app/index.js
@@ -96,6 +96,3 @@ function showMainWindow() {
 
   mainWindow.webContents.openDevTools()  
 }
-  
-
-  
