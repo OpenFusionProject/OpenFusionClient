5d5dce964741552156137b1bba4a024e2ed36ef4
CakeLancelot <CakeLancelot@users.noreply.github.com>
2021-04-28 12:37:22 -0500

    Move to npm for development and packaging
    I had to hack the package.json to force `electron-prebuilt` to use a 32-bit binary - everything else should be fairly out of the box

--- index.js
+++ index.js
@@ -9,6 +9,76 @@ var mainWindow = null;
 
 app.commandLine.appendSwitch('--enable-npapi');
 
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+
+function handleSquirrelEvent() {
+  "use strict"
+
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function(command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+    } catch (error) {}
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit();
+      return true;
+  }
+};
+
 // Node version is too old to have a built-in function
 function copyFile(src, dst) {
   fs.createReadStream(src).pipe(fs.createWriteStream(dst));
@@ -23,9 +93,9 @@ function initialSetup() {
   child.on('exit', function() {
     console.log("Unity installed.");
     // Copy over files with default values
-    copyFile(__dirname+"\\default_config.json", app.getPath('userData')+"\\config.json");
-    copyFile(__dirname+"\\default_servers.json", app.getPath('userData')+"\\servers.json");
-    copyFile(__dirname+"\\default_versions.json", app.getPath('userData')+"\\versions.json");
+    copyFile(__dirname+"\\defaults\\config.json", app.getPath('userData')+"\\config.json");
+    copyFile(__dirname+"\\default\\servers.json", app.getPath('userData')+"\\servers.json");
+    copyFile(__dirname+"\\default\\versions.json", app.getPath('userData')+"\\versions.json");
     console.log("JSON files copied.");
     setupWindow.destroy();
     showMainWindow();
@@ -94,5 +164,15 @@ function showMainWindow() {
     mainWindow.webContents.executeJavaScript("loadServerList();");
   });
 
+  mainWindow.webContents.on('plugin-crashed', function() {
+    console.log("Unity Web Player crashed.");
+  });
+
+  mainWindow.webContents.on('will-navigate', function(evt, url) {
+    evt.preventDefault();
+    console.log(url);
+  });
+
   //mainWindow.webContents.openDevTools()  
 }
+
