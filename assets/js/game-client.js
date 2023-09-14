d4dc403e90b58e65be3672618c58aa55f6809546
CakeLancelot <CakeLancelot@users.noreply.github.com>
2023-09-14 15:13:23 -0500

    Remove now unnecessary attributes from game-client.js

--- assets/js/game-client.js
+++ assets/js/game-client.js
@@ -80,14 +80,6 @@ function launchGame() {
         "progressframeimage",
         "assets/img/unity-loadingframe.png"
     );
-    embed.setAttribute(
-        "autoupdateurlsignature",
-        "42180ee5edc4e3d4dd706bcc17cedd8d6ec7b7ac463071fd34ab97fe181f1a78df31db5feb4526677e4f69ef53acaff44471591e68b87f041c80fd54765f0d5725b08aa28f5acf7716ffb2a04e971269f35925c7e38d57dd78f6a206530caaa3da7e32f07f19810efc0ebf29a4eae976a925ad9cc5beb4dd51564c67dc489033"
-    );
-    embed.setAttribute(
-        "autoupdateurl",
-        "http://wp-cartoonnetwork.unity3d.com/ff/big/beta-20111013/autodownload_webplugin_beta"
-    );
 
     var div = document.getElementById("client");
     object.appendChild(embed);
